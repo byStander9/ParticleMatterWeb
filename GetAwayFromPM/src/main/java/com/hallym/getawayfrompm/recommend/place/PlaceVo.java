29fac11a003beb89bd2753b46a3b35ea77477705
byStander <byStander@DESKTOP-PMJOLT0>
2023-12-10 10:07:11 +0900

    Add PlaceVo

--- GetAwayFromPM/src/main/java/com/hallym/getawayfrompm/recommend/place/PlaceVo.java
+++ GetAwayFromPM/src/main/java/com/hallym/getawayfrompm/recommend/place/PlaceVo.java
@@ -2,4 +2,34 @@ package com.hallym.getawayfrompm.recommend.place;
 
 public class PlaceVo {
 
+	private String place_name;
+	private String place_link;
+	private String place_roadAddress;
+	private String place_img;
+	public String getPlace_name() {
+		return place_name;
+	}
+	public void setPlace_name(String place_name) {
+		this.place_name = place_name;
+	}
+	public String getPlace_link() {
+		return place_link;
+	}
+	public void setPlace_link(String place_link) {
+		this.place_link = place_link;
+	}
+	public String getPlace_roadAddress() {
+		return place_roadAddress;
+	}
+	public void setPlace_roadAddress(String place_roadAddress) {
+		this.place_roadAddress = place_roadAddress;
+	}
+	public String getPlace_img() {
+		return place_img;
+	}
+	public void setPlace_img(String place_img) {
+		this.place_img = place_img;
+	}
+	
+	
 }
